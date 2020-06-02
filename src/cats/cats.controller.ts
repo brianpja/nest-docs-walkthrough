e9b8a6afd5d9d6405576f2a5bc6b09d825f40d22
Brian James <brian.james@slalom.com>
2020-06-02 10:15:54 -0700

    @Post method in cats controller

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Get, Req } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Get, Post } from '@nestjs/common';
+
 
 @Controller('cats')
 export class CatsController {
+  @Post()
+  create(): string {
+    return 'This action adds a new cat';
+  }
+
   @Get()
-  findAll(@Req() request: Request): string {
-    console.log('REQUEST', request);
+  findAll(): string {
     return 'This action returns all cats';
   }
 }
